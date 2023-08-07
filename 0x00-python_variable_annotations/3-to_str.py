f618ab37c7097f114677c5df529f064d46b12ba6
damadet <adedokundamilare41@gmail.com>
2023-08-07 17:11:00 +0200

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''convers a float to a string'''
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to a string
+
+    Parameters:
+      n(float): a float
+
+    Returns:
+      return_type: returns a string
+    """
+    return str(n)
