000fad2aa0ac7ce6e71718dc702c92fb95f65c88
damadet <adedokundamilare41@gmail.com>
2023-08-07 17:04:14 +0200

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''This takes in a float and returns a floor'''
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    This function returns an int
+
+    Paraameters:
+      n(float): this is a float number
+
+    Return:
+      return_type: int
+    """
+    return math.floor(n)
