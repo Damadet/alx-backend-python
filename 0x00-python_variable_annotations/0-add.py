d6ec66755d51ba2e1c3803e08dfa6e07b6f0eafe
damadet <adedokundamilare41@gmail.com>
2023-08-07 16:19:59 +0200

    0-add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def add(a: float, b: float) -> float:
+    """
+    This function adds two floats
+
+    Parameters:
+        a(float): A float number.
+        b(float): A float number.
+
+    Returns:
+        return_type: Returns a float.
+    """
     return a + b
