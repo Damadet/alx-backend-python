ecc1341e137e1dc095a283e31c671b1f6f9599dd
damadet <adedokundamilare41@gmail.com>
2023-08-07 16:58:05 +0200

    1-concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Concatenates two strings'''
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function concats two strings.
+
+    Parameters:
+      str1(str): a string
+      str2(str): another string
+
+    Returns:
+      return_type: returns a string
+    """
+    return str1 + str2
